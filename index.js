0c0e9e5ac8e9bae5b52de8efc5af80e206423480
swaaz <swaasthik.shetty07@gmail.com>
2021-07-08 22:41:55 +0530

    trying out things

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ app.get('/', (req , res) => {
     res.render('index');
 })
 
-app.listen('5000', () => console.log('Server is running..'))
+
+
+
+app.listen('5000', () => console.log('Server is running in port 5000..'))
