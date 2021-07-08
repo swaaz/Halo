37309864431dfd6179ba6486a0fc81037cb9185a
swaaz <swaasthik.shetty07@gmail.com>
2021-07-08 23:55:17 +0530

    added bg and made full screen

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ app.set('view engine', 'pug');
 app.get('/', (req , res) => {
     res.render('index');
 })
+app.get('/mulroom', (req , res) => {
+    res.render('mulroom');
+})
+app.get('/joinroom', (req , res) => {
+    res.render('joinroom');
+})
 
 app.listen('5000', () => console.log('Server is running..'))
