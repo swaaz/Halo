6dec914442a36db67ddc74f6bb5ae4a748eee669
swaaz <swaasthik.shetty07@gmail.com>
2021-07-10 21:26:19 +0530

    react set up is complete

---
+++ src/components/SinglePlayer/SinglePlayer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './SinglePlayer.css';
+const SinglePlayer = () => {
+    return (
+        <div>
+            <div class="gameContainer">
+                <div class="gameInfo">
+                    <h1 class="header">Halo </h1>
+                    <h4 class="header">Playing vs Bot </h4>
+                    <div class="score">Score</div>
+                </div>
+                <div class="GridContainer">
+                    <div class="box" id="box1"></div>
+                    <div class="box" id="box2"></div>
+                    <div class="box" id="box3"></div>
+                    <div class="box" id="box4"></div>
+                    <div class="box" id="box5"></div>
+                    <div class="box" id="box6"></div>
+                    <div class="box" id="box7"></div>
+                    <div class="box" id="box8"></div>
+                    <div class="box" id="box9"></div>
+                    <div class="box" id="box10"></div>
+                    <div class="box" id="box11"></div>
+                    <div class="box" id="box12"></div>
+                    <div class="box" id="box13"></div>
+                    <div class="box" id="box14"></div>
+                    <div class="box" id="box15"></div>
+                    <div class="box" id="box16"></div>
+                    <div class="box" id="box17"></div>
+                    <div class="box" id="box18"></div>
+                    <div class="box" id="box19"></div>
+                    <div class="box" id="box20"></div>
+                    <div class="box" id="box21"></div>
+                    <div class="box" id="box22"></div>
+                    <div class="box" id="box23"></div>
+                    <div class="box" id="box24"></div>
+                    <div class="box" id="box25"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SinglePlayer
