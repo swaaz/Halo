f386425155093408074072c324c0f0d71a2a5ee8
swaaz <swaasthik.shetty07@gmail.com>
2021-07-13 18:37:24 +0530

    working on bugs

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,6 @@
 const makeid = (length) => {
 	var result = '';
-	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 	var charactersLength = characters.length;
 	for (var i = 0; i < length; i++) {
 		result += characters.charAt(Math.floor(Math.random() * charactersLength));
