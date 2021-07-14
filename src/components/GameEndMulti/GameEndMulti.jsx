687e7c3eca6fc37d9ce65d076e7789ee61ba9760
RachithaRai <rachitharai3016@gmail.com>
2021-07-14 16:33:02 +0530

    fix multi gameover card

--- src/components/GameEndMulti/GameEndMulti.jsx
+++ src/components/GameEndMulti/GameEndMulti.jsx
@@ -4,16 +4,18 @@ import './GameEndMulti.css';
 const GameEndMulti = (props) => {
     console.log(props)
     return (
-        <div id="cardHeight" className="multiPlayerStart">
-            <div>GAME OVER :(</div>
+        <div id="cardHeight" className="multiend-container">
+            <div className="multiend">
+            <div className="multiend-over">GAME OVER :(</div>
             <div className="scoreList">
                 {
                     props.ranks.map((val,index) => <p>{index+1}. {val.name}</p>)
                 }
             </div>
-            <div class="game-end-options">
-                <div onClick={props.replay} id="replay" className="option">Replay</div>
-                <Link className="linkHome" to='/'><div className="option">Home</div></Link>
+            <div class="multiend-options">
+                <div onClick={props.replay} id="replay" className="multiend-option multiend-option1">Replay</div>
+                <Link className="multiend-linkHome" to='/'><div className="multiend-option multiend-option2">Home</div></Link>
+            </div>
             </div>
         </div>
     )
