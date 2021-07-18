fc6ecb370b70b043fdfd36d3912a6a4ed6aa56cf
suhanacharya <suhan.acharya1@gmail.com>
2021-07-18 09:27:31 +0530

    Added Instructions to the game

--- src/App.js
+++ src/App.js
@@ -9,23 +9,25 @@ import {
 import Multiplayer from './components/Multiplayer/Multiplayer';
 import LeaderBoard from './components/LeaderBoard/LeaderBoard';
 import About from './components/About/About';
+import Instructions from './components/Instructions/Instructions'
 
 function App() {
   return (
     <Router>
       <div>
-      <div id="stars1"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
+        <div id="stars1"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
         <Switch>
           <Route exact path='/leaderboard' component={LeaderBoard} />
+          <Route exact path='/instructions' component={Instructions} />
           <Route exact path='/about' component={About} />
           <Route exact path='/multi' component={Multiplayer} />
           <Route exact path='/single' component={SinglePlayer} />
           <Route path='/leader' component={LeaderBoard} />
-          <Route  path="/" component={Home} />
+          <Route path="/" component={Home} />
         </Switch>
-    </div>
+      </div>
     </Router>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
                 <div className="items">
                     <div className="item"> <Link className="home-link" to="/single">Single Player</Link></div>
                     <div className="item"> <Link className="home-link" to='/multi'>MultiPlayer </Link></div>
-                    <div className="item"> <Link className="home-link" to="/">Instructions </Link></div>
+                    <div className="item"> <Link className="home-link" to="/instructions">Instructions </Link></div>
                     <div className="itemlast">
                         <div className="iteml item1"> <Link to="/leader">LeaderBoard</Link> </div>
                         <div className="iteml item2"><Link to="/leader"> About</Link></div>
